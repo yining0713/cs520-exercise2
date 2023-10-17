daa1a7338e03e4b278c032ea747ea2b8d93df4c8
Mahbuba Tasmin <tasmin153@gmail.com>
2023-10-17 07:44:52 -0400

    Initial test script add

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Change to the directory containing the Python script
+cd "./test_suit"
+coverage run -m pytest test_triangle.py
+coverage report -m --include="test_triangle.py"
+coverage html -d initial_statement_html
+
+
+#!/bin/bash
+# Change to the directory containing the Python script
+
+coverage run --branch test_triangle.py
+coverage report -m --include="test_triangle.py"
+coverage html -d initial_coverage_html
+
+
+python.exe mut.py --target isTriangle --unit-test test_triangle -m 
+python.exe mut.py --target isTriangle --unit-test test_triangle -m --report-html initial_mutation_report.html 
