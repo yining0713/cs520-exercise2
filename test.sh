adddae52858fecd9ba3cd41f356e63bb508f0502
Heather Conboy <hconboy@cs.umass.edu>
2023-10-18 16:32:36 -0400

    Update test.sh
    Fixed a typo in the branch coverage folder name.

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ coverage html -d initial_statement_html
 
 coverage run --branch test_triangle.py
 coverage report -m --include="test_triangle.py"
-coverage html -d initial_coverage_html
+coverage html -d initial_decision_html
 
 
 python3 mut.py --target isTriangle --unit-test test_triangle -m 
