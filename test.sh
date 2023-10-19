681470953a6c502140ff3191a0599d1d77ebb787
Mahbuba Tasmin <tasmin153@gmail.com>
2023-10-18 21:40:28 -0400

    Randomization control introduced

--- test.sh
+++ test.sh
@@ -25,5 +25,13 @@ elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
     PYTHON_EXECUTABLE="python3.exe"
 fi
 
-$PYTHON_EXECUTABLE mut.py --target isTriangle --unit-test test_triangle -m 
-$PYTHON_EXECUTABLE mut.py --target isTriangle --unit-test test_triangle -m --report-html initial_mutation_report.html 
+# $PYTHON_EXECUTABLE mut.py --target isTriangle --unit-test test_triangle -m 
+# $PYTHON_EXECUTABLE mut.py --target isTriangle --unit-test test_triangle -m --report-html initial_mutation_report.html 
+
+# Define the output file for both stdout and stderr
+output_file="initial_mutation_output.log"
+# Use tee to capture both stdout and stderr and save them to the file
+{
+    $PYTHON_EXECUTABLE mut.py --target isTriangle --unit-test test_triangle -m  
+    $PYTHON_EXECUTABLE mut.py --target isTriangle --unit-test test_triangle -m --report-html initial_mutation_report.html
+} 2>&1 | tee "$output_file"
