d00c3960c18f6557496668aab8dde8c69f0844f8
yiningzhu0713 <yiningzhu@umass.edu>
2023-10-26 18:33:47 -0400

    remove redundant decision coverage tests

--- test_suit/test_decisionCoverage.py
+++ test_suit/test_decisionCoverage.py
@@ -5,46 +5,45 @@ from isTriangle import Triangle
 # from src.Triangle import Triangle
 
 class DecisionCoverageTest(unittest.TestCase):
-    def test1(self):
+    def test1(self): # line 39: true 51%
         actual = Triangle.classify(10, 10, 10)
         expected = Triangle.Type.EQUILATERAL
         self.assertEqual(actual, expected)
 
-    def test2(self):
-        actual = Triangle.classify(1, 1, 10)
+    def test2(self): # line 21: true 55%
+        actual = Triangle.classify(-1, 0, 0)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test3(self):
+    def test3(self): # line 26: true 67%
         actual = Triangle.classify(8, 8, 10)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
     
-    def test4(self):
-        actual = Triangle.classify(8, 9, 10)
+    def test4(self): # line 28: true 76%
+        actual = Triangle.classify(8, 10, 8)
         expected = Triangle.Type.SCALENE
         self.assertEqual(actual, expected)
 
-    def test5(self):
-        actual = Triangle.classify(8, -1, 10)
+    def test5(self): # line 30: true 84%
+        actual = Triangle.classify(10, 8, 8)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test6(self):
+    def test6(self): # line 34: true 92%
         actual = Triangle.classify(8, 1, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test7(self):
-        actual = Triangle.classify(8, 5, 5)
+    def test7(self): # line 36: true 96%
+        actual = Triangle.classify(8, 5, 4)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
     
-    def test8(self):
-        actual = Triangle.classify(8, 5, 8)
-        expected = Triangle.Type.ISOSCELES
+    def test8(self): # line 46 false: 100%
+        actual = Triangle.classify(8, 8, 20)
+        expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
-
 
 if __name__ == '__main__':
     unittest.main()
