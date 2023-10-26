96c1a04edfddfdae12e67f0788e60e3c40c84c7b
yiningzhu0713 <yiningzhu@umass.edu>
2023-10-26 18:24:20 -0400

    remove redundant statement coverage tests

--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -5,66 +5,45 @@ from isTriangle import Triangle
 # from src.Triangle import Triangle
 
 class StatementCoverageTest(unittest.TestCase):
-    def test1(self):
+    def test1(self): # line 22
         actual = Triangle.classify(-8, 10, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test2(self):
+    def test2(self): # line 40 equilateral 68%
         actual = Triangle.classify(10, 10, 10)
         expected = Triangle.Type.EQUILATERAL
         self.assertEqual(actual, expected)
 
-    def test3(self):
+    def test3(self): # line 36/37 scalene 74%
         actual = Triangle.classify(8, 9, 10)
         expected = Triangle.Type.SCALENE
         self.assertEqual(actual, expected)
 
-    def test4(self):
+    def test4(self): # line 34/35 invalid inequality 77%
         actual = Triangle.classify(1, 7, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def test5(self):
+    def test5(self): # line 42/43 a==b ISOSCELES 84%
         actual = Triangle.classify(5, 5, 8)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
 
-    def test6(self):
+    def test6(self): # line 44/45 a==c ISOSCELES 90%
         actual = Triangle.classify(5, 8, 5)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
 
-    def test7(self):
+    def test7(self): # line 46/47 b==c ISOSCELES 97%
         actual = Triangle.classify(8, 5, 5)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
 
-    def test8(self):
+    def test8(self): # line 49 two sides equal, but invalid 
         actual = Triangle.classify(11, 5, 5)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
-
-    def test9(self):
-        actual = Triangle.classify(10, -8, 10)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test10(self):
-        actual = Triangle.classify(10, 10, -8)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test11(self):
-        actual = Triangle.classify(2, 10, 1)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
-    def test12(self):
-        actual = Triangle.classify(10, 2, 1)
-        expected = Triangle.Type.INVALID
-        self.assertEqual(actual, expected)
-
 
 if __name__ == '__main__':
     unittest.main()
